dc3a5ae116f7e5d23108040deab6b8ebb6cdab6f
Igor.Kornilov <gaber32188@gmail.com>
2021-09-25 19:33:07 +0300

    add ts node + ts config

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ mongoose.connect(db_url, {});
 const db = mongoose.connection;
 db.on('error', (err) => console.error(err));
 
-db.once('open', () => console.log(`db connected || ${process.env.NODE_ENV.toUpperCase()}`));
+db.once('open', () => console.log(`db connected || ${process.env.NODE_ENV?.toUpperCase()}`));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`serv started on port ${PORT}`));
