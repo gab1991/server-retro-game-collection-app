d403b22558cbc50f76f54d554492f7df6d0d0d8f
Igor.Kornilov <gaber32188@gmail.com>
2021-09-26 18:48:11 +0300

    move db to db folder

---
+++ db/index.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const db_url = global.__IS_PROD__ ? process.env.DATABASE_CLOUD_URL : process.env.DATABASE_URL;
+
+if (!db_url) {
+  throw new Error('database url has not been provided');
+}
+
+mongoose.connect(`${db_url}`, {});
+
+const db = mongoose.connection;
+
+db.on('error', (err) => console.error(err));
+
+db.once('open', () => console.info(`db connected || ${process.env.NODE_ENV?.toUpperCase()}`));
--- server.ts
+++ server.ts
@@ -1,21 +1,7 @@
 import './env';
 import './globals';
-import mongoose from 'mongoose';
+import './db';
 import { app } from './app';
-
-const db_url = global.__IS_PROD__ ? process.env.DATABASE_CLOUD_URL : process.env.DATABASE_URL;
-
-if (!db_url) {
-  throw new Error('database url has not been provided');
-}
-
-mongoose.connect(`${db_url}`, {});
-
-const db = mongoose.connection;
-
-db.on('error', (err) => console.error(err));
-
-db.once('open', () => console.info(`db connected || ${process.env.NODE_ENV?.toUpperCase()}`));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.info(`serv started on port ${PORT}`));
