f7402a8439cbeaf56b6e1e1a9ec91b2866f568eb
Igor.Kornilov <gaber32188@gmail.com>
2021-10-10 15:45:35 +0300

    add inline img to cps

--- src/app.ts
+++ src/app.ts
@@ -36,7 +36,7 @@ app.use(
       useDefaults: true,
       directives: {
         'font-src': ['*'],
-        'img-src': ['*'],
+        'img-src': ['*', 'data:'],
       },
       // reportOnly: true,
     },
